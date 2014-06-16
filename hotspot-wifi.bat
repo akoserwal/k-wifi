c4b4064aa13dcca31e829ce52506e20bc1b6ee2e
Abhishek koserwal <koserwaliet@gmail.com>
2014-06-16 22:04:40 +0530

    first commit

---
+++ hotspot-wifi.bat
@@ -0,0 +1,6 @@
+SET /P ssid_name=Please Enter Ad-Hoc Name:-
+SET /P key_name=Please Enter Key Value:-
+netsh wlan set hostednetwork mode=allow ssid= %ssid_name%  key=%key_name%
+netsh wlan start hostednetwork
+
+
