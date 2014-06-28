71d2c7a9c0b71dba26d81c0cee478b789770a32a
Abhishek koserwal <abhishek.koserwal@gmail.com>
2014-06-28 19:09:00 +0530

    ADD keyUsage

--- k-wifi.bat
+++ k-wifi.bat
@@ -1,6 +1,7 @@
+ECHO off
 SET /P ssid_name=Please Enter Ad-Hoc Name:-
 SET /P key_name=Please Enter Key Value:-
-netsh wlan set hostednetwork mode=allow ssid= %ssid_name%  key=%key_name%
+netsh wlan set hostednetwork mode=allow ssid= %ssid_name%  key=%key_name% keyUsage=persistent
 netsh wlan start hostednetwork
 
 
